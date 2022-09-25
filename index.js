8c1e39f359120e19ea5bba964ae8ee28de96f216
sean w mckenzie <seanwyattmckenzie@gmail.com>
2022-09-25 06:59:42 -0800

    Fixed drums background images

--- index.js
+++ index.js
@@ -2,6 +2,10 @@ const myDrums = document.querySelectorAll(".drum")
 
 for(i=0; i < myDrums.length; i++){
     myDrums[i].addEventListener("click", function(){
-        alert("button has been clicked")
+    this.style.color = "white"
     })
 }
+
+
+// var aduio = new Audio("sounds/crash.mp3")
+// aduio.play()
